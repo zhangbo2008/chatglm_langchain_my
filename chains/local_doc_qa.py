e532d104b569613c3341d9e7f17d23436f91b8bf
zhangbo2008 <15122306087@163.com>
2023-06-09 09:18:51 +0800

    no_comment

--- chains/local_doc_qa.py
+++ chains/local_doc_qa.py
@@ -101,6 +101,7 @@ def generate_prompt(related_docs: List[str],
                     prompt_template: str = PROMPT_TEMPLATE, ) -> str:
     context = "\n".join([doc.page_content for doc in related_docs])
     prompt = prompt_template.replace("{question}", query).replace("{context}", context)
+    print('你当前的问题, 底层的prompt是',prompt)
     return prompt
 
 
